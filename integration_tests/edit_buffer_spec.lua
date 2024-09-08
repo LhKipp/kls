e80197de644fbc9350544817f5d7646660127941
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2024-09-08 19:40:50 +0200

    Add tests

--- integration_tests/edit_buffer_spec.lua
+++ integration_tests/edit_buffer_spec.lua
@@ -29,4 +29,44 @@ async.describe("DidChangeNotification", function()
         local scopes = client.print_scopes({ print_file_contents = true })
         golden_spec:test("did_change__add_new_text"):is_expected(scopes)
     end)
+
+    async.it("track code edits", function()
+        local client = require "kserver".start("did_change__change_text",
+            {
+                files = {
+                    ["src/main/kotlin/example.kt"] = "package example.com",
+                }
+            }
+        )
+        vim.cmd.edit("src/main/kotlin/example.kt")
+        exec_keys("$Bcwmypackage<ESC>")
+        -- TODO, neovim is not sending the didChange notification without the write
+        vim.cmd.write()
+
+        -- assert exec_keys worked
+        assert.equal(vim.api.nvim_buf_get_lines(0, 0, 1, false)[1], 'package mypackage.com')
+
+        local scopes = client.print_scopes({ print_file_contents = true })
+        golden_spec:test("did_change__change_text"):is_expected(scopes)
+    end)
+
+    async.it("track code edits", function()
+        local client = require "kserver".start("did_change__delete_text",
+            {
+                files = {
+                    ["src/main/kotlin/example.kt"] = "package example.com",
+                }
+            }
+        )
+        vim.cmd.edit("src/main/kotlin/example.kt")
+        exec_keys("dd")
+        -- TODO, neovim is not sending the didChange notification without the write
+        vim.cmd.write()
+
+        -- assert exec_keys worked
+        assert.equal(vim.api.nvim_buf_get_lines(0, 0, 1, false)[1], '')
+
+        local scopes = client.print_scopes({ print_file_contents = true })
+        golden_spec:test("did_change__delete_text"):is_expected(scopes)
+    end)
 end)
